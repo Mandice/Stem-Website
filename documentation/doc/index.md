77b744ce2d8f7b7ae674a7d70ce970c859fc69b9
Fred Chien <fred@mandice.com>
2013-04-06 18:43:55 +0800

    Add documentation page

---
+++ documentation/doc/index.md
@@ -0,0 +1,12 @@
+
+### Manual & Documentation
+
+
+#### Table of Contents
+
+
+* General
+	- Installation
+* Development
+	- Quick Example
+	- Package application
---
+++ routes/router/doc.js
@@ -0,0 +1,39 @@
+
+var fs = require('fs');
+var stream = require('stream');
+var path = require('path');
+
+module.exports = {
+	'/doc': doc,
+	'/doc/:topic': doc
+};
+
+function doc(req, res) {
+	var buffer = [];
+
+	var topic = 'index';
+	if (req.params.topic) {
+		topic = path.basename(req.params.topic);
+	}
+
+	var docPath = res.app.locals.configs.documentationPath + '/doc/' + topic + '.md';
+
+	var stream = fs.createReadStream(docPath);
+	stream.setEncoding('utf8');
+
+	stream.on('error', function(error) {
+		console.error(error.toString());
+	});
+
+	stream.on('data', function(data) {
+		buffer.push(data);
+	});
+
+	stream.on('end', function() {
+		var content = buffer.join('');
+
+		res.locals.markdown = res.app.locals.markdown;
+		res.locals.currentPath = req.path;
+		res.render('doc', { title: 'Stem Operating System', content: content });
+	});
+}
