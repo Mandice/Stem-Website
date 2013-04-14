55c13ca27acac00a63aeaf86260fbc1d24017f90
Fred Chien <fred@mandice.com>
2013-04-14 17:47:49 +0800

    add support page

---
+++ documentation/support.md
@@ -0,0 +1,13 @@
+
+# Support
+
+Stem is free of charge, anyone can download its installation directly for general version of Stem from our official website. Most of information you can get from online documentation or community.
+
+We will do our best to support this project and help people to use it. If you would like to support us, we hope that you will consider making a donation to Stem.
+
+## Customization and Development Support
+
+You might have own hardware and product required to run Stem, but general version of Stem doesn't work on. There are actually third-party vendors provided customization service for enabling you devices and development support.
+
+* Mandice (Taiwan)
+* Symbio (USA, Finland, China, Taiwan)
--- routes/router/index.js
+++ routes/router/index.js
@@ -4,7 +4,8 @@ var stream = require('stream');
 
 module.exports = {
 	'/': index,
-	'/about': about
+	'/about': about,
+	'/support': support
 };
 
 function index(req, res) {
@@ -33,5 +34,28 @@ function about(req, res) {
 		res.locals.markdown = res.app.locals.markdown;
 		res.locals.currentPath = req.path;
 		res.render('about', { title: 'Stem Operating System', content: content });
+	});
+}
+
+function support(req, res) {
+	var buffer = [];
+
+	var stream = fs.createReadStream(res.app.locals.configs.documentationPath + '/support.md');
+	stream.setEncoding('utf8');
+
+	stream.on('error', function(error) {
+		console.error(error.toString());
+	});
+
+	stream.on('data', function(data) {
+		buffer.push(data);
+	});
+
+	stream.on('end', function() {
+		var content = buffer.join('');
+
+		res.locals.markdown = res.app.locals.markdown;
+		res.locals.currentPath = req.path;
+		res.render('support', { title: 'Stem Operating System', content: content });
 	});
 }
